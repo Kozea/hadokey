5d7d109e90e5b36c2c472ba462fb7d29b854dd47
Jessyca Hervieux <jessyca.hervieux@kozea.fr>
2014-04-11 16:01:10 +0200

    correction of bug for graphic per hour

--- ok.py
+++ ok.py
@@ -1,33 +1,29 @@
-#IMPORTS
 import os
 import sqlite3
 import calendar
 import locale
 from datetime import datetime, timedelta
 from pygal import Line, Bar, DateY, Gauge
-from flask import Flask, request, session, g, redirect,  url_for, abort,\
-render_template, flash
+from flask import (
+    Flask, request, session, g, redirect,  url_for, abort, render_template,
+    flash)
+
 
 locale.setlocale(locale.LC_ALL, 'fr_FR')
 
-#APPLICATION
+
 app = Flask(__name__)
-app.config.from_object(__name__)
-
-#CONFIG
 app.config.update(dict(
-    DATABASE=os.path.join(app.root_path, 'ok.db'),
-    DEBUG=True,
-    SECRET_KEY='ok'
-))
-app.config.from_envvar('FLASKR_SETTINGS', silent=True)
+    DATABASE=os.path.join(app.root_path, 'ok.db'), DEBUG=True,
+    SECRET_KEY='ok'))
 
-#DATABASE
+
 def connect_db():
-    """Connects to the specific database."""
-    rv = sqlite3.connect(app.config['DATABASE'])
-    rv.row_factory = sqlite3.Row
-    return rv
+    """Connect to the specific database."""
+    conn = sqlite3.connect(app.config['DATABASE'])
+    conn.row_factory = sqlite3.Row
+    return conn
+
 
 def init_db():
     with app.app_context():
@@ -36,25 +32,40 @@ def init_db():
             db.cursor().executescript(f.read())
         db.commit()
 
+
 def get_db():
-    """Opens a new database connexion if there is non yet for the current
-    application context."""
+    """Open a new database connexion.
+    
+    If the connection already exists, this connection is returned.
+    
+    """
     if not hasattr(g, 'sqlite_db'):
-        g.sqlite_db=connect_db()
+        g.sqlite_db = connect_db()
     return g.sqlite_db
+
 
 @app.teardown_appcontext
 def close_db(error):
-    """Closes the database again at the end of the request."""
+    """Close the database again at the end of the request."""
     if hasattr(g, 'sqlite_db'):
         g.sqlite_db.close()
 
-#FUNCTIONS
+
 @app.route('/', methods=['GET', 'POST'])
 def insert_ok():
+    """Allow to insert hickup in the database.
+    
+    If the date is not past, the insertion is impossible
+    and an error message is returned.
+    
+    """
     db = get_db()
-    nbOk = None if db.execute('select count(*) from ok').fetchone()[0] == 0 else db.execute('select count(*) from ok').fetchone()[0]
-    message = "Cette date n'est pas encore passée. On ne sait toujours pas si Annabelle aura le hoquet à ce moment là..."
+    nb_ok = None if db.execute(
+        'select count(*) from ok').fetchone()[0] == 0 else db.execute(
+        'select count(*) from ok').fetchone()[0]
+    message = (
+        "Cette date n'est pas encore passée. On ne sait toujours pas si "
+        "Annabelle aura le hoquet à ce moment là...")
     
     if request.method =='POST':
         now = datetime.now()
@@ -69,77 +80,121 @@ def insert_ok():
 
         timestamp = datetime(*(date + time)).timestamp()
         if timestamp >= now.timestamp():
-                return render_template('insert_ok.html', nbOk=nbOk, message=message)            
+            return render_template(
+                'insert_ok.html', nb_ok=nb_ok, message=message)            
         cur = db.execute('insert into ok (moment) values(?)', [timestamp])
         db.commit()
         flash('Vous avez inséré un nouveau \'hoquet\'')
         return redirect(url_for('show_ok'))
-    return render_template('insert_ok.html', nbOk=nbOk)
+    return render_template('insert_ok.html', nb_ok=nb_ok)
+
 
 @app.route('/view')
-@app.route('/view/<int:year>/<int:month>')
-@app.route('/view/<date>')
-def show_ok(date=None, year=None, month=None):
-    now = datetime.now()
-    month = month or now.month
-    year = year or now.year
-    title = calendar.month_name[month]
-    cal = calendar.Calendar(firstweekday=0)   
-    daysweek = calendar.day_name
-    days = cal.itermonthdates(year, month)
-    link = now.strftime('%Y-%m-')
-    db = get_db()
-            
-    if date is None:
+@app.route('/view/<int:year>-<int:month>')
+@app.route('/view/<int:year>-<int:month>-<int:day>')
+def show_ok(year=None, month=None, day=None):
+    """Allow to view hickup by hour for a specific day.
+        
+        It's possible to navigate between the months by entering a specific 
+        date or on click.            
+                
+    """
+    def create_calendar(year, month):
+        """Allow to view hickup by hour for a specific day."""
+        title = calendar.month_name[month]
+        cal = calendar.Calendar(firstweekday=0)   
+        days_week = calendar.day_name
+        days = cal.itermonthdates(year, month)
+        return (title, days_week, days)
+     
+    if not year and not month:
+        year = datetime.now().year
+        month = datetime.now().month
+        title, days_week, days = create_calendar(year, month)
         return render_template(
-            'show_ok.html', days=days, title=title, daysweek=daysweek, link=link, month=month, year=year)
-    date = [int(i) for i in date.split('-')]
-    table = datetime(date[0], date[1], date[2])
-    frenchDate = table.strftime('%d %B %Y')
-    start = datetime(*(date)).timestamp()
-    ok_list = db.execute('select moment from ok where moment >= (?) and moment <= (?) order by moment', [start, start+86400])
-    ok_list = [datetime.fromtimestamp(ok['moment']).strftime('%H:%M') for ok in ok_list]
-    return render_template(
-        'show_ok.html', days=days, title=title, daysweek=daysweek, ok_list=ok_list, 
-        frenchDate=frenchDate, link=link, month=month, year=year)
+            'show_ok.html', days=days, title=title, days_week=days_week,
+            month=month, year=year, day=day)
+               
+    title, days_week, days = create_calendar(year, month)
 
+    if day:
+        french_date = datetime(year, month, day).strftime('%d %B %Y')
+        start = datetime(year, month, day).timestamp()
+        db = get_db()
+        ok_list = db.execute(
+            'select moment from ok where moment >= (?) and moment <= (?) '
+            'order by moment', [start, start+86400])
+        ok_list = [datetime.fromtimestamp(
+            ok['moment']).strftime('%H:%M') for ok in ok_list]
+        return render_template(
+            'show_ok.html', days=days, title=title, days_week=days_week, 
+            ok_list=ok_list, french_date=french_date, month=month, 
+            year=year, day=day)
+            
+    return render_template(
+            'show_ok.html', days=days, title=title, days_week=days_week,
+            month=month, year=year)
+            
+                 
 @app.route('/graphics', methods=['GET', 'POST'])
 def show_graphics():
+    """Allow to view graphics per month of hickup.
+        
+       You can see them par day or per hour.
+                
+    """
     if request.method == 'POST':
         graph = request.form['graphic']
         type = 'hour' if request.form['display'] == 'hour' else 'day'
         year, month, day = [int(i) for i in request.form['month'].split('-')]
-        return render_template('show_graphics.html', graph=graph, type=type, month=month, year=year)
+        return render_template(
+            'show_graphics.html', graph=graph, type=type, month=month, 
+            year=year)
     return render_template('show_graphics.html')
+
 
 @app.route('/graph/<graph>/<type>/<int:year>/<int:month>')
 def graph(graph, type, year, month):
+    """Create graphs with the parameters passed."""
     db = get_db()
     now = datetime.now()
     timestamp = now.timestamp()
-    firstday = datetime(year, month, 1).timestamp()
+    first_day = datetime(year, month, 1).timestamp()
     cal = calendar.monthrange(year, month)
-    lastday = datetime(year, month, cal[1], 23, 59, 59).timestamp()
+    last_day = datetime(year, month, cal[1], 23, 59, 59).timestamp()
     
-    if graph in ["ligne", "histogramme"]:
-        line_chart = Line() if graph == "ligne" else Bar()
+    if graph in ['ligne', 'histogramme']:
+        line_chart = Line() if graph == 'ligne' else Bar()
         if type == 'hour':
-            line_chart.title = 'Nombre de hoquets maximum par heure en %s' % calendar.month_name[month]   
-            requete = db.execute('select strftime(\'%H\', datetime(moment, \'unixepoch\', \'localtime\')) as hour, count(*) from ok where moment between (?) and (?) group by hour', [firstday, lastday])
+            line_chart.title = (
+                'Somme des hoquets par heure en %s' % 
+                calendar.month_name[month])
+            requete = db.execute(
+                'select CAST(strftime(\'%H\', datetime(moment, \'unixepoch\','
+                ' \'localtime\')) as integer) as hour, count(*) from ok '
+                'where moment between (?) and (?) group by hour', 
+                [first_day, last_day])
             hoquet = dict(requete)
-            line_chart.x_labels = map(str, range(9, 19))
-            line_chart.add('Annabelle', hoquet)
+            line_chart.x_labels = [
+               str(hour) for hour, val in sorted(hoquet.items())]
+            line_chart.add('Annabelle', [
+                val for hour, val in sorted(hoquet.items())])
         else:
             hoquet = []
-            line_chart.title = "Nombre de hoquets par jour en %s" % calendar.month_name[month]
-            for day in range (int(firstday), int(timestamp), 86400):
-                requete = db.execute('select count(*) from ok where moment >= (?) and moment <= (?)', [day, day+86400]).fetchone()[0]
+            line_chart.title = (
+                'Nombre de hoquets par jour en %s' % 
+                calendar.month_name[month])
+            for day in range (int(first_day), int(timestamp), 86400):
+                requete = db.execute(
+                    'select count(*) from ok where moment >= (?) '
+                    'and moment <= (?)', [day, day+86400]).fetchone()[0]
                 hoquet.append(requete)
             line_chart.x_labels = map(str, range(cal[0], cal[1]+1))
             line_chart.add('Annabelle', hoquet)
         return line_chart.render_response()
             
         return gauge_chart.render_response()
-        
+
+
 if __name__ == '__main__':
     app.run()
